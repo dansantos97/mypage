f5f3a10d594c6115beacfed9536505e4d909508a
Daniel Ferreira dos Santos <89503051+dansantos97@users.noreply.github.com>
2022-08-11 02:09:39 -0300

    Update styles.js

--- src/components/About/styles.js
+++ src/components/About/styles.js
@@ -1,17 +1,24 @@
 import styled from "styled-components";
 
 export const AboutText = styled.div`
-  @keyframes fadeIn {
+   @keyframes fadeIn {
     0% { opacity: 0; }
     100% { opacity: 1; }
   }
+
   z-index: 10;
   display: flex;
   padding-left: 15px;
+
+  @media (max-width: 620px) {
+    padding-left: 5px;
+  }
+
   img {
     animation: fadeIn 3s ease-in-out;
     max-width: 500px;
     border-radius: 4px;
+
     @media (max-width: 1100px) {
         display: none;
       }
@@ -20,12 +27,22 @@ export const AboutText = styled.div`
   h2 {
     color: #dfa70c;
     text-align: center;
+
+    @media (max-width: 620px) {
+      margin-bottom: 0px;
+      margin-top: 0px;
+    }
   }
 
   p {
     color: #ededed;
     line-height: 1.5rem;
     text-align: justify;
+
+    @media (max-width: 620px) {
+      line-height: 15px;
+      margin-bottom: 0;
+    }
   }
 
   div.Text{
