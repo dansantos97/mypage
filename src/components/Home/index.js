741282ecaed1c8028668a84b37af11f433a268d2
Daniel Ferreira dos Santos <89503051+dansantos97@users.noreply.github.com>
2022-08-11 02:05:28 -0300

    Add files via upload

---
+++ src/components/Home/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { HomeText } from "./styles";
+
+export default function Home() {
+  return (
+      <HomeText>
+        <h2 className="name">Daniel Ferreira dos Santos</h2>
+        <h1>Front End Developer</h1>
+        <h2>Bem-vindo à minha página!</h2>
+        <p>"Programadores e artistas são os únicos profissionais que têm como hobby a própria profissão"  - Rafael Lain</p>
+      </HomeText>
+
+  )
+}
