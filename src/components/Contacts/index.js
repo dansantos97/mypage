4fb920704cbf0101346ee965627ffd19c27f4a77
Daniel Ferreira dos Santos <danferreira.santos@hotmail.com>
2023-09-11 22:00:49 -0300

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: modified:   src/components/Contacts/index.js

--- src/components/Contacts/index.js
+++ src/components/Contacts/index.js
@@ -7,19 +7,19 @@ export default function Contacts() {
       <h1>Entre em contato comigo!</h1>
       <ul>
         <li className="email">
-          <a href="mailto:danferreira.santos@hotmail.com" target='_blank' rel="noreferrer">danferreira.santos@hotmail.com</a>
+          <a href="mailto:danferreira.santos@hotmail.com" target='_blank'>danferreira.santos@hotmail.com</a>
         </li>
         <li className="linkedin">
-          <a href="https://www.linkedin.com/in/dansantosdev" target='_blank' rel="noreferrer">Visite meu Linkedin</a>
+          <a href="https://www.linkedin.com/in/dansantosdev" target='_blank'>Visite meu Linkedin</a>
         </li>
         <li className="github">
-          <a href="https://github.com/dansantos97" target='_blank' rel="noreferrer">Acesse meu perfil no Github</a>
+          <a href="https://github.com/dansantos97" target='_blank'>Acesse meu perfil no Github</a>
         </li>
         <li className="insta">
-          <a href="https://www.instagram.com/danferreirarj" target='_blank' rel="noreferrer">Visite meu Instagram</a>
+          <a href="https://www.instagram.com/danferreirarj" target='_blank'>Visite meu Instagram</a>
         </li>
         <li className="whatsapp">
-          <a href="https://api.whatsapp.com/send?phone=5521980154830">Tel/WhatsApp: (21)98015-4830</a>
+          <a href="https://api.whatsapp.com/send?phone=5521980154830" target='_blank'>Tel/WhatsApp: (21)98015-4830</a>
         </li>
       </ul>
     </ContactsContainer>
