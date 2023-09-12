b614ca094f6c73988800b380fd43da341e40e2d4
Daniel Ferreira dos Santos <danferreira.santos@hotmail.com>
2023-09-11 22:26:45 -0300

    modified:

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <HomeText>
       <h2 className="name">Daniel Ferreira dos Santos</h2>
-      <h1>Front End Developer</h1>
+      <h1>Full Stack Developer</h1>
       <h2>Bem-vindo à minha página!</h2>
       <p>"Programadores e artistas são os únicos profissionais que têm como hobby a própria profissão"  - Rafael Lain</p>
     </HomeText>
